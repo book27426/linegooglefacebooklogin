a4ddb3daeddc0880a1602ee9d287f4526c706f61
Npass <b.book0601@gmail.com>
2026-01-19 09:42:12 +0700

    fixcookies

--- app/resultv/page.js
+++ app/resultv/page.js
@@ -5,7 +5,7 @@ import ClientProfile from './client-profile';
 export default async function Page() {
   const cookieStore = await cookies();   // ✅ REQUIRED
   const sessionCookie = cookieStore.get('session');
-  const sessionToken = sessionCookie.value;
+  const sessionToken = sessionCookie?.value;
   const user = await getUserfromid(sessionToken);
   if (!user) {
     return <div>User not found</div>;
